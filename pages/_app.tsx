4ea76a27417ebb6dacb3bd81ee6dbd8b0ad403be
Jimbo Jayson <illegaljson@gmail.com>
2022-08-08 21:32:13 -0500

    Initialize firebase app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const firebaseConfig = {
+  apiKey: process.env.API_KEY || "",
+  authDomain: process.env.AUTH_DOMAIN || "",
+  databaseURL: process.env.DB_URL || "",
+  projectId: process.env.PROJECT_ID || "",
+  storageBucket: process.env.STORAGE_BUCKET || "",
+  messagingSenderId: process.env.SENDER_ID || "",
+  appId: process.env.APP_ID || "",
+  measurementId: process.env.MEASUREMENT_ID || "",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
